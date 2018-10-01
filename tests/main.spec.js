2aed419705f17806125e25975af56e17cee65451
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-10-01 10:01:24 -0300

    Add smoke tests

---
+++ tests/main.spec.js
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import {
+  search, searchAlbuns, searchArtists, searchTracks, searchPlaylists,
+} from '../src/main';
+
+describe('Spotify Wrapper', () => {
+
+  describe('smoke tests', () => {
+    // search (genérico) = + de 1 tipo
+    // searchAlbuns
+    // searchArtists
+    // searchTracks
+    // searchPlaylists
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbuns method', () => {
+      expect(searchAlbuns).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+});
