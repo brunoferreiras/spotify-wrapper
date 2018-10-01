1c8ac18f58d0e8c1acfc45cee58d1a16a7c63764
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-10-01 10:06:19 -0300

    Add sinon and dependencies

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -32,4 +32,11 @@ describe('Spotify Wrapper', () => {
       expect(searchPlaylists).to.exist;
     });
   });
+
+  describe('Generic Search', () => {
+
+    it('should call fetch function', () => {
+      const artists = search();
+    });
+  });
 });
